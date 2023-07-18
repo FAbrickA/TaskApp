a30f411b497578a043844f4ce88e70d4e71236b4
FAbrickA <schilonosov.alex@yandex.ru>
2023-07-18 15:57:14 +0300

    Test Auth endpoints

--- taskapi/tests/test_endpoints/test_auth.py
+++ taskapi/tests/test_endpoints/test_auth.py
@@ -1,11 +1,30 @@
-async def test_ping(client):
-    response = await client.get("api/v1/ping")
-    assert response.status_code == 200
-    data = response.text
-    assert data.lower() == "pong"
+from fastapi import status
+
+from api.v1.endpoints.auth.schemas import UserData, Token
+from api.v1.endpoints.auth.utils import decode_token
+from db.models import User
 
 
-async def test_test_db(client):
-    response = await client.get("api/v1/test_db")
-    print(response.text)
-    assert response.status_code == 200
+class TestAuthToken:
+    url = "/api/v1/auth/token"
+
+    async def test_get_token(self, client, user_sample: tuple[User, str]):
+        user, raw_password = user_sample
+
+        # make request to get token
+        user_data = UserData(
+            email=user.email,
+            password=raw_password,
+        )
+        response = await client.post(self.url, json=user_data.model_dump())
+        assert response.status_code == status.HTTP_200_OK
+
+        # extract token and check its type
+        data = response.json()
+        token = Token(**data)
+        assert token.token_type.lower() == "bearer"
+
+        # check access token
+        payload = decode_token(token.access_token)
+        assert "email" in payload
+        assert payload.get("email") == user.email
